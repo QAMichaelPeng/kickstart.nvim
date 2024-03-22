83f698659fa733bca1695857a075142bdc59f0f5
QAMichaelPeng <pengfeng.git@gmail.com>
2024-03-21 21:37:26 -0700

    Remove opt of Vista

--- init.lua
+++ init.lua
@@ -840,7 +840,7 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
-  { 'liuchengxu/vista.vim', opt = {} },
+  { 'liuchengxu/vista.vim' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
