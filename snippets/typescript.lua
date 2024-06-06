62372beefdae901c4b98a2ffd5201885b34734f9
QAMichaelPeng <pengfeng.git@gmail.com>
2024-06-05 22:02:09 -0700

    Fix eslint snippet

--- snippets/typescript.lua
+++ snippets/typescript.lua
@@ -1,9 +1,16 @@
+local get_eslint_rules = function()
+  return {
+    t '@typescript-eslint/no-unused-vars',
+    t '@typescript-eslint/consistent-type-assertions',
+  }
+end
 return {
-  s(
-    'eld',
-    c(1, {
-      t '@typescript-eslint/no-unused-vars',
-      t '@typescript-eslint/consistent-type-assertions',
-    })
-  ),
+  s('eld', {
+    t '// eslint-disable ',
+    c(1, get_eslint_rules()),
+  }),
+  s('eldn', {
+    t '// eslint-disable-next-line ',
+    c(1, get_eslint_rules()),
+  }),
 }
