8a684ad122b307ec3c136a736e11be872f014663
QAMichaelPeng <pengfeng.git@gmail.com>
2024-11-27 07:52:58 -0800

    Add trouble.nvim

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -62,6 +62,7 @@ require('lazy').setup({
   require 'kickstart.plugins.rustaceanvim',
   require 'kickstart.plugins.spelunk',
   require 'kickstart.plugins.neotest',
+  require 'kickstart.plugins.trouble',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
