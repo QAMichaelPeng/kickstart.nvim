ffd6d55b5741712905540da8bc150a6afa70930e
QAMichaelPeng <pengfeng.git@gmail.com>
2024-11-27 07:41:25 -0800

    Add neotest

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -61,6 +61,7 @@ require('lazy').setup({
   require 'kickstart.plugins.auto-session',
   require 'kickstart.plugins.rustaceanvim',
   require 'kickstart.plugins.spelunk',
+  require 'kickstart.plugins.neotest',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
